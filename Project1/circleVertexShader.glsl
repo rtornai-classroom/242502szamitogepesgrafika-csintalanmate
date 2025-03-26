68b73f75e714ddf2a404e12183c0770b381297f7
csintalanmate <125445719+csintalanmate@users.noreply.github.com>
2025-03-27 00:15:01 +0100

    Elso projekt kesz

---
+++ Project1/circleVertexShader.glsl
@@ -0,0 +1,25 @@
+#version 330 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
+
+out vec3 fragColor;
+
+uniform vec2 circleCenter;
+uniform bool colorSwap; // Érintkezésnél szín csere
+
+void main() {
+    // Az uniform értékének megfelelõen mozgatjuk a kört
+    gl_Position = vec4(position.x + circleCenter.x, position.y + circleCenter.y, position.z, 1.0);
+    
+    // Ha kell cserél színt
+    if (colorSwap) {
+        if (color.g > color.r) {  // If originally green (outer part)
+            fragColor = vec3(1.0f, 0.0f, 0.0f); // Pirosra vált
+        } else { // If originally red (center)
+            fragColor = vec3(0.0f, 1.0f, 0.0f); // Zöldre vált
+        }
+    } else {
+        fragColor = color;
+    }
+}
